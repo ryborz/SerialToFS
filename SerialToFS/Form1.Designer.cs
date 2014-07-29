6a658f2366092ae4b34071480d91e442f9184c3d
unknown <fryborz@gmail.com>
2014-07-29 18:28:38 +0200

    Initial commit

---
+++ SerialToFS/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace SerialToFS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.bConnect = new System.Windows.Forms.Button();
+            this.cbPort1 = new System.Windows.Forms.ComboBox();
+            this.rtb1 = new System.Windows.Forms.RichTextBox();
+            this.sp1 = new System.IO.Ports.SerialPort(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.bGetCoverage = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbBaud = new System.Windows.Forms.TextBox();
+            this.tbTrigger = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bConnect
+            // 
+            this.bConnect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bConnect.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.bConnect.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(219)))), ((int)(((byte)(7)))));
+            this.bConnect.Location = new System.Drawing.Point(12, 130);
+            this.bConnect.Name = "bConnect";
+            this.bConnect.Size = new System.Drawing.Size(100, 23);
+            this.bConnect.TabIndex = 0;
+            this.bConnect.Text = "Connect";
+            this.bConnect.UseVisualStyleBackColor = true;
+            this.bConnect.Click += new System.EventHandler(this.bConnect_Click);
+            // 
+            // cbPort1
+            // 
+            this.cbPort1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.cbPort1.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.cbPort1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(119)))), ((int)(((byte)(0)))));
+            this.cbPort1.FormattingEnabled = true;
+            this.cbPort1.Location = new System.Drawing.Point(12, 25);
+            this.cbPort1.Name = "cbPort1";
+            this.cbPort1.Size = new System.Drawing.Size(100, 23);
+            this.cbPort1.TabIndex = 1;
+            // 
+            // rtb1
+            // 
+            this.rtb1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtb1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.rtb1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.rtb1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(119)))), ((int)(((byte)(0)))));
+            this.rtb1.Location = new System.Drawing.Point(12, 171);
+            this.rtb1.Name = "rtb1";
+            this.rtb1.Size = new System.Drawing.Size(376, 185);
+            this.rtb1.TabIndex = 2;
+            this.rtb1.Text = "";
+            // 
+            // sp1
+            // 
+            this.sp1.BaudRate = 115200;
+            this.sp1.ReadBufferSize = 2048;
+            this.sp1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.sp1_DataReceived);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // bGetCoverage
+            // 
+            this.bGetCoverage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.bGetCoverage.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bGetCoverage.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.bGetCoverage.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(219)))), ((int)(((byte)(7)))));
+            this.bGetCoverage.Location = new System.Drawing.Point(239, 130);
+            this.bGetCoverage.Name = "bGetCoverage";
+            this.bGetCoverage.Size = new System.Drawing.Size(149, 23);
+            this.bGetCoverage.TabIndex = 3;
+            this.bGetCoverage.Text = "Get Coverage";
+            this.bGetCoverage.UseVisualStyleBackColor = true;
+            this.bGetCoverage.Click += new System.EventHandler(this.bGetCoverage_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(204)))), ((int)(((byte)(204)))));
+            this.label1.Location = new System.Drawing.Point(12, 155);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Output:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(204)))), ((int)(((byte)(204)))));
+            this.label2.Location = new System.Drawing.Point(9, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "COM port:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(204)))), ((int)(((byte)(204)))));
+            this.label3.Location = new System.Drawing.Point(9, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Settings:";
+            // 
+            // tbBaud
+            // 
+            this.tbBaud.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.tbBaud.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.tbBaud.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(119)))), ((int)(((byte)(0)))));
+            this.tbBaud.Location = new System.Drawing.Point(12, 65);
+            this.tbBaud.Name = "tbBaud";
+            this.tbBaud.Size = new System.Drawing.Size(100, 21);
+            this.tbBaud.TabIndex = 7;
+            this.tbBaud.Text = "115200,8,N,1";
+            // 
+            // tbTrigger
+            // 
+            this.tbTrigger.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.tbTrigger.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.tbTrigger.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(119)))), ((int)(((byte)(0)))));
+            this.tbTrigger.Location = new System.Drawing.Point(12, 104);
+            this.tbTrigger.Name = "tbTrigger";
+            this.tbTrigger.Size = new System.Drawing.Size(100, 21);
+            this.tbTrigger.TabIndex = 9;
+            this.tbTrigger.Text = "x";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(204)))), ((int)(((byte)(204)))));
+            this.label4.Location = new System.Drawing.Point(9, 88);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Trigger:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(41)))), ((int)(((byte)(41)))));
+            this.ClientSize = new System.Drawing.Size(400, 368);
+            this.Controls.Add(this.tbTrigger);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbBaud);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bGetCoverage);
+            this.Controls.Add(this.rtb1);
+            this.Controls.Add(this.cbPort1);
+            this.Controls.Add(this.bConnect);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Serial to File system";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bConnect;
+        private System.Windows.Forms.ComboBox cbPort1;
+        private System.Windows.Forms.RichTextBox rtb1;
+        private System.IO.Ports.SerialPort sp1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button bGetCoverage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbBaud;
+        private System.Windows.Forms.TextBox tbTrigger;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ SerialToFS/Form1.cs
@@ -0,0 +1,226 @@
+﻿// <author>Filip Ryborz</author>
+// <date>07/21/2014</date>
+// <summary>Program handling file operations when interfacing embedded system by serial port.</summary>
+// <website>simply-embedded.blogspot.com</website>
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SerialToFS
+{
+    public partial class Form1 : Form
+    {
+        private Byte[] rxBuf;
+        private Byte[] txBuf;
+        private Byte[] fileBuf;
+        private int fileIdx;
+        private int fileSize;
+        private System.IO.FileStream file;
+
+        public Form1()
+        {
+            InitializeComponent();
+            rxBuf = new Byte[1024];
+            txBuf = new Byte[1024];
+            fileBuf = new Byte[4096];
+            file = null;
+            fileIdx = 0;
+            fileSize = 0;
+            foreach (string s in System.IO.Ports.SerialPort.GetPortNames())
+            {
+                cbPort1.Items.Add(s);
+            }
+        }
+
+        private void bConnect_Click(object sender, EventArgs e)
+        {
+            string sTmp = tbBaud.Text;
+            try
+            {
+                sp1.PortName = cbPort1.Items[cbPort1.SelectedIndex].ToString();
+                sp1.BaudRate = int.Parse(sTmp.Substring(0, sTmp.IndexOf(',')));
+                sTmp = sTmp.Substring(sTmp.IndexOf(',') + 1);
+                sp1.DataBits = int.Parse(sTmp.Substring(0, sTmp.IndexOf(',')));
+                sTmp = sTmp.Substring(sTmp.IndexOf(',') + 1);
+                switch (sTmp.Substring(0, sTmp.IndexOf(',')))
+                {
+                    case "N":
+                    case "n":
+                        sp1.Parity = System.IO.Ports.Parity.None;
+                        break;
+                    case "E":
+                    case "e":
+                        sp1.Parity = System.IO.Ports.Parity.Even;
+                        break;
+                    case "O":
+                    case "o":
+                        sp1.Parity = System.IO.Ports.Parity.Odd;
+                        break;
+                    case "M":
+                    case "m":
+                        sp1.Parity = System.IO.Ports.Parity.Mark;
+                        break;
+                    case "S":
+                    case "s":
+                        sp1.Parity = System.IO.Ports.Parity.Space;
+                        break;
+                    default:
+                        sp1.Parity = System.IO.Ports.Parity.None;
+                        break;
+                }
+                sTmp = sTmp.Substring(sTmp.IndexOf(',') + 1);
+                switch (sTmp)
+                {
+                    case "0":
+                        sp1.StopBits = System.IO.Ports.StopBits.None;
+                        break;
+                    case "1":
+                        sp1.StopBits = System.IO.Ports.StopBits.One;
+                        break;
+                    case "1.5":
+                    case "1,5":
+                        sp1.StopBits = System.IO.Ports.StopBits.OnePointFive;
+                        break;
+                    case "2":
+                        sp1.StopBits = System.IO.Ports.StopBits.Two;
+                        break;
+                    default:
+                        sp1.StopBits = System.IO.Ports.StopBits.One;
+                        break;
+                }
+                sp1.Open();
+                cbPort1.Enabled = false;
+                tbBaud.Enabled = false;
+                bConnect.Enabled = false;
+            }
+            catch (Exception ex) { MessageBox.Show(ex.ToString()); }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                sp1.Close();
+                file.Close();
+            }
+            catch (Exception ex) { ex.ToString(); }
+        }
+
+        private void sp1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            int rxLen = sp1.BytesToRead;
+            int tmp = 0;
+
+
+            if (rxLen > 0)
+            {
+                if (fileSize > 0)
+                {
+
+                    if (rxLen > fileSize)
+                    {
+                        tmp = rxLen;
+                        rxLen = fileSize;
+                    }
+                    sp1.Read(fileBuf, fileIdx, rxLen);
+                    fileIdx += rxLen;
+                    fileSize -= rxLen;
+                    rxLen = tmp;
+                }
+                if (fileSize == 0)
+                {
+                    //rxLen = sp1.BytesToRead; 
+                    sp1.Read(rxBuf, 0, rxLen);
+                    rtb1.Invoke(new EventHandler(delegate { rtb1.AppendText(System.Text.Encoding.ASCII.GetString(rxBuf, 0, rxLen)); }));
+                }
+
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            String sFile = "";
+            String sTmp = "";
+            int size = 0;
+            int readSize = 0;
+
+            if (rtb1.Lines.Length > 0)
+            {
+                sTmp = rtb1.Lines[rtb1.Lines.Length - 1];
+                // open file
+                if (sTmp.IndexOf("fOpen(") > -1)
+                {
+                    sFile = sTmp.Substring(sTmp.IndexOf('(') + 1, sTmp.IndexOf(',') - sTmp.IndexOf('(') - 1);
+                    try
+                    {
+                        file = System.IO.File.Open(sFile, System.IO.FileMode.OpenOrCreate);
+                    }
+                    catch (Exception ex) { ex.ToString(); }
+                    if (file != null)
+                    {
+                        txBuf[0] = 0;
+                        txBuf[1] = 0;
+                        sp1.Write(txBuf, 0, 2);
+                        rtb1.AppendText("\n\rFile opened!\n\r");
+                    }
+                    else
+                    {
+                        txBuf[0] = 0xFF;
+                        txBuf[1] = 0xFF;
+                        sp1.Write(txBuf, 0, 2);
+                        rtb1.AppendText("\n\rFile not opened!\n\r");
+                    }
+                }
+                // read file
+                if ((sTmp.IndexOf("fRead") > -1) && (file != null))
+                {
+                    readSize = int.Parse(sTmp.Substring(sTmp.IndexOf(", ") + 1, sTmp.IndexOf('|') - sTmp.IndexOf(',') - 1));
+                    size = (int)file.Length;
+                    txBuf[0] = (Byte)((size & 0xFF00) >> 8);
+                    txBuf[1] = (Byte)(size & 0x00FF);
+                    sp1.Write(txBuf, 0, 2);
+                    rtb1.AppendText("\n\rReading file!\n\r");
+                    if (readSize < size)
+                    {
+                        size = readSize;
+                    }
+                    file.Read(fileBuf, 0, size);
+                    sp1.Write(fileBuf, 0, size);
+                }
+                // write file
+                if ((sTmp.IndexOf("fWrite") > -1) && (file != null))
+                {
+                    fileSize = int.Parse(sTmp.Substring(sTmp.IndexOf(", ") + 1, sTmp.IndexOf('|') - sTmp.IndexOf(',') - 1));
+                    sp1.Write(txBuf, 0, 1); // send ACK
+                    rtb1.AppendText("\n\rWriting file!\n\r");
+                }
+                // close file
+                if ((sTmp.IndexOf("fClose") > -1) && (file != null))
+                {
+                    file.Position = 0;
+                    file.Write(fileBuf, 0, fileIdx);
+                    fileIdx = 0;
+
+                    file.Close();
+                    sp1.Write(txBuf, 0, 1); // send ACK
+                    rtb1.AppendText("\n\rFile closed!\n\r");
+                }
+            }
+
+        }
+
+        private void bGetCoverage_Click(object sender, EventArgs e)
+        {
+            if (tbTrigger.Text.Length > 0)
+            {
+                txBuf[0] = (byte)tbTrigger.Text[0];
+                sp1.Write(txBuf, 0, 1); // send ACK
+            }
+        }
+    }
+}
